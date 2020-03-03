6d43f3d5f1e17003ba32abbc948fcd67fde2383d
dhruv dwivedi <dhruv.dwivedi@3ds.com>
2020-03-03 13:26:12 +0530

    rails test

---
+++ rails/myblog/db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Post.destroy_all
+Category.destroy_all
+
+Category.create(title: "Programming")
+Category.create(title: "Web Development")
+Category.create(title: "RoR")
+Category.create(title: "React")
+
+Category.all.each do |cat|
+    3.times do |i|
+        Post.create(title: "Post #{i}", body: "Post body", category:cat)
+    end
+end
