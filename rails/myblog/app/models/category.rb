20e6b968691371f3594c58b05f3eb8b1461e9f01
dhruv dwivedi <dhruv.dwivedi@3ds.com>
2020-03-03 17:42:59 +0530

    devise test for login

--- rails/myblog/app/models/category.rb
+++ rails/myblog/app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
     has_many :posts
+    validates :title, presence: true
 end
--- rails/myblog/config/routes.rb
+++ rails/myblog/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :installs
   resources :posts
   resources :categories
   root "posts#index"
